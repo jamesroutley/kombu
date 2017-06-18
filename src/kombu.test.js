4ce289f4b2336c13b425cfc41a5efa450e72362d
James Routley <jroutley@gmail.com>
2017-06-18 18:26:06 -0400

    Fix eslint warnings

--- src/kombu.test.js
+++ src/kombu.test.js
@@ -4,29 +4,29 @@ test('flattenRows', () => {
     const runs = [
         {input: [['a'], ['b']], output: ['a', 'b']},
         {input: [['a', 'b'], ['c']], output: [['a', 'b'], ['c']]}
-    ]
+    ];
     runs.forEach((run) => {
         expect(kombu._flattenRows(run.input)).toEqual(run.output);
-    })
+    });
 });
 
 test('flattenCols', () => {
     const runs = [
         {input: [['a', 'b']], output: ['a', 'b']},
         {input: [['a', 'b'], ['c']], output: [['a', 'b'], ['c']]}
-    ]
+    ];
     runs.forEach((run) => {
         expect(kombu._flattenCols(run.input)).toEqual(run.output);
-    })
-})
+    });
+});
 
 test('_isTwiceNestedArray', () => {
     const runs = [
         {input: ['a'], output: false},
         {input: [['a'], 'b'], output: false},
         {input: [['a'], ['b']], output: true}
-    ]
+    ];
     runs.forEach((run) => {
         expect(kombu._isTwiceNestedArray(run.input)).toEqual(run.output);
-    })
-})
+    });
+});
